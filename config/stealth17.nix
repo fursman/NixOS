6bfe4b9bf988f7ce967fe7b3110ea5f83d05d8e6
fursman <120282785+fursman@users.noreply.github.com>
2023-11-27 02:24:53 -0800

    Update stealth17.nix

--- config/stealth17.nix
+++ config/stealth17.nix
@@ -221,6 +221,4 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
 }
