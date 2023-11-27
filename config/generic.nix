35524ad35dd4bc695c1c3e653587d22996557275
fursman <120282785+fursman@users.noreply.github.com>
2023-11-27 14:46:55 -0800

    Update generic.nix made generic

--- config/generic.nix
+++ config/generic.nix
@@ -13,16 +13,16 @@
   };
 
   # Enable plymouth
-  boot.plymouth.enable = true;
+  # boot.plymouth.enable = true;
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.systemd.enable = true;
   boot.initrd.verbose = false;
-  boot.loader.timeout = 0;
-  boot.consoleLogLevel = 0;
-  boot.kernelParams = ["quiet" "splash"];
+  # boot.loader.timeout = 0;
+  # boot.consoleLogLevel = 0;
+  # boot.kernelParams = ["quiet" "splash"];
 
   boot.supportedFilesystems = [ "ntfs" ];
 
@@ -122,36 +122,6 @@
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
-  };
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = false;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # Allow unfree packages
