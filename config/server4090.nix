ad33219433f11b2000f03567e681e3e816b7a98a
fursman <120282785+fursman@users.noreply.github.com>
2023-12-16 12:39:19 -0800

    Update server4090.nix changed hostname to block

--- config/server4090.nix
+++ config/server4090.nix
@@ -48,7 +48,7 @@
 
   boot.supportedFilesystems = [ "ntfs" ];
 
-  networking.hostName = "NixOS"; # Define your hostname.
+  networking.hostName = "Block"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
