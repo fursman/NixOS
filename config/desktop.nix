8afa8e1c9ae6efc4d4de34fd23f382b7de938c7b
fursman <120282785+fursman@users.noreply.github.com>
2023-12-16 16:31:30 -0800

    Update desktop.nix added wayland to gdm

--- config/desktop.nix
+++ config/desktop.nix
@@ -54,6 +54,7 @@
   services.xserver.enable = true;
 
   # Enable Greeter
+  services.xserver.displayManager.gdm.wayland = true;
   services.xserver.displayManager.gdm.enable = true;
 
   # Enagle the gnome-keyring
