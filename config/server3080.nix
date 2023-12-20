c2b948a01217f7f725892c37b672759925b8840a
fursman <120282785+fursman@users.noreply.github.com>
2023-12-19 21:02:57 -0800

    Update server3080.nix

--- config/server3080.nix
+++ config/server3080.nix
@@ -234,7 +234,7 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 7860 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
