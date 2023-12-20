65337d59d2024ce2b6c0ddb2f72968c570ba0b02
fursman <120282785+fursman@users.noreply.github.com>
2023-12-19 17:28:59 -0800

    Update server4090.nix changed Nvidia driver to beta

--- config/server4090.nix
+++ config/server4090.nix
@@ -179,7 +179,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   # Allow unfree packages
