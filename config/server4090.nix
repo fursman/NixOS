e8bd114bc708e615bb0d7edb01bd60fe0afac7a1
fursman <120282785+fursman@users.noreply.github.com>
2023-12-19 20:52:28 -0800

    Update server4090.nix

--- config/server4090.nix
+++ config/server4090.nix
@@ -47,12 +47,6 @@
 
   boot.supportedFilesystems = [ "ntfs" ];
 
-  networking = {
-    hostName = "Block"; # Define your hostname.
-    firewall.allowedTCPPorts = [ 7860 ];
-  };
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -214,10 +208,15 @@
   };
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 7860 ];
+  # networking.firewall.allowedTCPPorts = [ 7860 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+
+  networking = {
+    hostName = "Block"; # Define your hostname.
+    firewall.allowedTCPPorts = [ 7860 ];
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
