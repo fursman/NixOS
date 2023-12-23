1bf0ca8a7adb078793f56dc9c5761357d5f7d439
fursman <120282785+fursman@users.noreply.github.com>
2023-12-22 18:52:00 -0800

    Update flake.nix Sha256

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
     };
     server3080Nix = builtins.fetchurl {
       url = "https://raw.githubusercontent.com/fursman/NixOS/main/config/server3080.nix";
-      sha256 = "0jkmb8q8z8yg80ylcd23cqbgyck2sd0sx9x46gygxfjb90wyr82i0jkmb8q8z8yg80ylcd23cqbgyck2sd0sx9x46gygxfjb90wyr82i";
+      sha256 = "0jkmb8q8z8yg80ylcd23cqbgyck2sd0sx9x46gygxfjb90wyr82i";
     };
     server4090Nix = builtins.fetchurl {
       url = "https://raw.githubusercontent.com/fursman/NixOS/main/config/server4090.nix";
