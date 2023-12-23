1a4b3324d8e99847ae04927efe744e109e01ac5f
fursman <120282785+fursman@users.noreply.github.com>
2023-12-22 18:49:54 -0800

    Update server3080.nix changed to docker

--- config/server3080.nix
+++ config/server3080.nix
@@ -206,10 +206,10 @@
 
   # Virtualization Settings
   virtualisation = {
-    podman = { # OCI Containers Settings
+    docker = { # OCI Containers Settings
       enable = true;
       # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
+      # dockerCompat = true;
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
       # Nvidia Enabled
