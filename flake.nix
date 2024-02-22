1fadb6efdafae807779a2d7c891d061840ecff00
fursman <120282785+fursman@users.noreply.github.com>
2024-02-21 16:10:37 -0800

    Reconfigured to a single file including former desktop.nix content

--- flake.nix
+++ flake.nix
@@ -25,10 +25,6 @@
       url = "https://raw.githubusercontent.com/fursman/NixOS/main/config/spotlight-dark.rasi";
       sha256 = "0ns89bqh8y23nwqij4da9wbas4x00l9mb66j769d8a5yy6hr4hzn";
     };
-    desktopNix = builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/fursman/NixOS/main/config/desktop.nix";
-      sha256 = "1yxs0ln5c7zc5r2naf53gcavqpzgpj1qvgr43hdlgig4awxq19bb";
-    };
     server3080Nix = builtins.fetchurl {
       url = "https://raw.githubusercontent.com/fursman/NixOS/main/config/server3080.nix";
       sha256 = "1gphalasxd7znrw6jrqsg1s85yjhixv715rapa2jc6zhw47kpw0r";
@@ -49,7 +45,6 @@
       url = "https://raw.githubusercontent.com/fursman/NixOS/main/config/yoga.nix";
       sha256 = "0g46zr6kwqcbjc4xixf8c5hfrwa5mhi8s5l3x0i8i2hwrrgxb9gd";
     };
-
     sharedConfiguration = ({ config, pkgs, ... }: {
       imports = [
         home-manager.nixosModules.home-manager
@@ -629,6 +624,222 @@
       environment.etc."assets".target = "assets";
     });
 
+    # Define the desktop configuration inline, previously contained in desktop.nix
+    desktopConfiguration = { config, pkgs, ... }: {
+      nix = {
+        package = pkgs.nixFlakes;
+        extraOptions = ''
+          experimental-features = nix-command flakes
+        '';
+      };
+      # Enable plymouth
+
+  boot.plymouth = {
+    enable = true;
+    theme = "abstract_ring";
+    themePackages = [(pkgs.adi1090x-plymouth-themes.override {selected_themes = ["abstract_ring"];})];
+  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.systemd.enable = true;
+  boot.initrd.verbose = false;
+  boot.loader.timeout = 0;
+  boot.consoleLogLevel = 0;
+  boot.kernelParams = [ "quiet" "splash" ];
+
+  boot.supportedFilesystems = [ "ntfs" ];
+
+  networking.hostName = "NixOS"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Bluethooth
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Vancouver";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable Greeter
+  services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.displayManager.gdm.enable = true;
+
+  # Enagle the gnome-keyring
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
+
+  # Enable the GNOME Desktop Environment.
+  # services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable Mouse in Hyprland
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+
+  # Enable Hyprlnd
+  programs.hyprland.enable = true;
+
+  # Enable xfce dbus communication (for Thunar)
+  services.dbus.packages = with pkgs; [
+    xfce.xfconf
+  ];
+
+  # Fonts Packages
+  fonts.packages = with pkgs; [
+    font-awesome
+  ];
+
+  # Enable Fonts
+  fonts.fontconfig.enable = true;
+
+  # Enable GVfs
+  services.gvfs.enable = true;
+
+  # Enable VS Code for Wayland
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # Enable PAM Config for Swaylock-effects
+  security.pam.services.swaylock = {
+    text = ''
+      auth include login
+    '';
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    ntfs3g
+  ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.user = {
+    isNormalUser = true;
+    description = "user";
+    extraGroups = [ 
+      "networkmanager"
+      "wheel"
+      "libvirtd" #Necessary for virt-manager
+    ];
+  };
+
+  # Virtualization Settings
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu.ovmf.enable = false;
+    };
+  };
+  programs.virt-manager.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+  };
   in
   {
     nixosConfigurations = {
@@ -636,7 +847,7 @@
         system = "x86_64-linux";
         modules = [
           ./hardware-configuration.nix
-          desktopNix
+          desktopConfiguration
           sharedConfiguration
         ];  
       };  
