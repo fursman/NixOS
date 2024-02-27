2359e78a79b00e6c50ccf28cadba2ae2cc39113b
fursman <120282785+fursman@users.noreply.github.com>
2024-02-26 21:24:45 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -439,8 +439,8 @@
             shadow_render_power = 3
             col.shadow = rgba(1a1a1aee)
             rounding = 5
-            active_opacity = 0.95
-            inactive_opacity = 0.85
+            active_opacity = 1
+            inactive_opacity = 0.9
         }
         
         animations {
