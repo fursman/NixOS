385c9ca286977765c8713806b68a9954ac2d7221
fursman <120282785+fursman@users.noreply.github.com>
2024-03-02 17:09:47 -0800

    Update pi4.nix

--- config/pi4.nix
+++ config/pi4.nix
@@ -9,13 +9,7 @@
     [
       <nixos-hardware/raspberry-pi/4>
     ];
-  hardware = {
-    raspberry-pi."4".apply-overlays-dtmerge.enable = true;
-    deviceTree = {
-      enable = true;
-      filter = "*rpi-4-*.dtb";
-    };
-  };
+
   console.enable = false;
 
   nix = {
