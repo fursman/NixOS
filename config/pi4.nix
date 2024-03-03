7f07dca70577bcc02e6ba59ddc6fb9c29dad6d2a
fursman <120282785+fursman@users.noreply.github.com>
2024-03-03 13:04:48 -0800

    Update pi4.nix

--- config/pi4.nix
+++ config/pi4.nix
@@ -23,7 +23,7 @@
 
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
-    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" "fbcon=rotate:1" ];
     loader = {
       grub.enable = false;
       generic-extlinux-compatible.enable = true;
