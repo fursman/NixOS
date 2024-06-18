4789ca6f83eb035252402d101b24e9bde0f2f426
fursman <120282785+fursman@users.noreply.github.com>
2024-06-17 19:25:52 -0700

    Update serverTitanV.nix

--- config/serverTitanV.nix
+++ config/serverTitanV.nix
@@ -43,7 +43,7 @@
   boot.loader.timeout = 0;
   boot.consoleLogLevel = 0;
   # Enable "quiet" output, "splash" screen and virtualization GPU options at boot
-  boot.kernelParams = [ "quiet" "splash" "intel_iommu=on" ];
+  boot.kernelParams = [ "quiet" "splash" "intel_iommu=on"  "iommu=pt" ];
 
   boot.supportedFilesystems = [ "ntfs" ];
 
@@ -78,7 +78,7 @@
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.displayManager.gdm.enable = true;
 
-  # Enagle the gnome-keyring
+  # Enable the gnome-keyring
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.greetd.enableGnomeKeyring = true;
 
@@ -151,7 +151,34 @@
   };
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["intel"];
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -175,6 +202,8 @@
       "wheel"
       "libvirtd" #Necessary for virt-manager
       "plugdev"
+      "kvm"
+      "qemu-libvirtd"
     ];
   };
 
