2b87be52b46fa4fab8dad17440eeecf58b6fc9e7
fursman <120282785+fursman@users.noreply.github.com>
2024-06-21 11:02:14 -0700

    Update server3080.nix changed dri to hardware

--- config/server3080.nix
+++ config/server3080.nix
@@ -144,9 +144,9 @@
   # services.xserver.libinput.enable = true;
 
   # Enable OpenGL
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
   };
 
   # Load nvidia driver for Xorg and Wayland
