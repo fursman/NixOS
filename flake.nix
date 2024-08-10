168bdec8ebdd1981ce59ef979bc46e3e7c6ea43a
fursman <120282785+fursman@users.noreply.github.com>
2024-08-10 15:19:03 -0700

    Update flake.nix removed explicitly enabling sound

--- flake.nix
+++ flake.nix
@@ -774,7 +774,6 @@
       services.printing.enable = true;
     
       # Enable sound with pipewire.
-      sound.enable = true;
       hardware.pulseaudio.enable = false;
       security.rtkit.enable = true;
       services.pipewire = {
