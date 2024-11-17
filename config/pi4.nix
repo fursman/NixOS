74754c9b6746fd0fa1caba7f3794016be01e22f6
fursman <120282785+fursman@users.noreply.github.com>
2024-11-17 11:12:49 -0800

    Update pi4.nix    package = pkgs.nixVersions.stable;

--- config/pi4.nix
+++ config/pi4.nix
@@ -13,7 +13,7 @@
   console.enable = true;
 
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nixVersions.stable;
     extraOptions = ''
         experimental-features = nix-command flakes
     '';
