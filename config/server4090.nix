7882fe80f220cf4c83601bb72eb7822265480f9e
fursman <120282785+fursman@users.noreply.github.com>
2025-03-26 13:46:17 -0700

    Update server4090.nix added IVSHMEM support

--- config/server4090.nix
+++ config/server4090.nix
@@ -1,18 +1,18 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
+  # Import the KVMFR module options (and its build derivation, see kvmfr-package.nix)
+  imports = [
+    ./kvmfr-options.nix
+  ];
+
   nix = {
     package = pkgs.nixVersions.stable;
     extraOptions = ''
-        experimental-features = nix-command flakes
+      experimental-features = nix-command flakes
     '';
   };
 
-  # These modules are required for PCI passthrough, and must come before early modesetting stuff
   boot.kernelModules = [
     "vfio_pci"
     "vfio"
@@ -20,17 +20,16 @@
     "vfio_virqfd"
   ];
   
-  # CHANGE: Don't forget to put your own PCI IDs here (run lspci -nn and look for NVIDIA)
-  boot.extraModprobeConfig ="options vfio-pci ids=10de:2684,10de:22ba";
+  boot.extraModprobeConfig = "options vfio-pci ids=10de:2684,10de:22ba";
 
-  # Enable plymouth
   boot.plymouth = {
     enable = true;
     theme = "red_loader";
-    themePackages = [(pkgs.adi1090x-plymouth-themes.override {selected_themes = ["red_loader"];})];
+    themePackages = [
+      (pkgs.adi1090x-plymouth-themes.override { selected_themes = [ "red_loader" ]; })
+    ];
   };
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.systemd.enable = true;
@@ -41,76 +40,50 @@
 
   boot.supportedFilesystems = [ "ntfs" ];
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Bluethooth
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Vancouver";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_CA.UTF-8";
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable Greeter
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.displayManager.gdm.enable = true;
 
-  # Enagle the gnome-keyring
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.greetd.enableGnomeKeyring = true;
 
-  # Enable the GNOME Desktop Environment.
-  # services.xserver.desktopManager.gnome.enable = true;
-
-  # Enable Hyprlnd
   programs.hyprland.enable = true;
-
-  # Enable Mouse in Hyprland
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
-  # Enable xfce dbus communication (for Thunar)
   services.dbus.packages = with pkgs; [
     xfce.xfconf
   ];
 
-  # Fonts Packages
   fonts.packages = with pkgs; [
     font-awesome
   ];
 
-  # Enable Fonts
   fonts.fontconfig.enable = true;
-
-  # Enable GVfs
   services.gvfs.enable = true;
 
-  # Enable PAM Config for Swaylock-effects
   security.pam.services.swaylock = {
     text = ''
       auth include login
     '';
   };
 
-  # Configure keymap in X11
   services.xserver = {
     xkb.layout = "us";
     xkb.variant = "";
   };
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -118,28 +91,15 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Enable OpenGL
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     git
     ntfs3g
@@ -148,20 +108,18 @@
     looking-glass-client
   ];
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.user = {
     isNormalUser = true;
     description = "user";
     extraGroups = [ 
       "networkmanager"
       "wheel"
-      "libvirtd" #Necessary for virt-manager
+      "libvirtd"  # Needed for virt-manager
       "kvm"
       "qemu-libvirtd"
     ];
   };
 
-  # Virtualization Settings
   virtualisation = {
     libvirtd = {
       enable = true;
@@ -169,34 +127,30 @@
       onShutdown = "shutdown";
       qemu = {
         package = pkgs.qemu_kvm;
-        ovmf = {
-          enable = true;
-        };
+        ovmf = { enable = true; };
         swtpm.enable = true;
         runAsRoot = false;
       };
     };
-    # USB redirection in virtual machine
-    spiceUSBRedirection.enable = true;
-  };
 
-  systemd.tmpfiles.rules = [
-    "f /dev/shm/looking-glass 0777 user qemu-libvirtd -"
-  ];
+    spiceUSBRedirection.enable = true;
+
+    # KVMFR configuration:
+    kvmfr = {
+      enable = true;
+      shm = {
+        enable = true;
+        size = 128;
+        user = "j-brn";
+        group = "libvirtd";
+        mode = "0600";
+      };
+    };
+  };
 
   programs.dconf.enable = true;
   programs.virt-manager.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-  # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
     extraConfig = ''
@@ -205,23 +159,10 @@
     '';
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ 7860 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   networking = {
-    hostName = "Block"; # Define your hostname.
+    hostName = "Block";
     firewall.allowedTCPPorts = [ 7860 8080 ];
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
+  system.stateVersion = "24.05";
 }
