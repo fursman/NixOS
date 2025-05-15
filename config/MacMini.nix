2ad6c743e30d8ce5380fd3655d8c22f775f77a99
fursman <120282785+fursman@users.noreply.github.com>
2025-05-14 18:29:01 -0700

    Create MacMini.nix

---
+++ config/MacMini.nix
@@ -0,0 +1,216 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  nix = {
+    package = pkgs.nixVersions.stable;
+    extraOptions = ''
+        experimental-features = nix-command flakes
+    '';
+  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.systemd.enable = true;
+  boot.initrd.verbose = true;
+  boot.loader.timeout = 0;
+  boot.consoleLogLevel = 0;
+  boot.kernelParams = [ 
+    "snd-intel-dspcfg.dsp_driver=1" 
+    "snd_hda_intel.probe_mask=1" 
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.blacklistedKernelModules = [ "snd_sof_intel_hda_common" "snd_sof_intel_hda" "snd_sof_pci" "snd_sof_xtensa_dsp" ];
+
+  boot.supportedFilesystems = [ "ntfs" ];
+
+  networking.hostName = "MacMini-Nix"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # RTL-SDR
+  hardware.rtl-sdr.enable = true;
+
+  # Bluethooth
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Vancouver";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable Greeter
+  services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.displayManager.gdm.enable = true;
+
+  # Enagle the gnome-keyring
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
+
+  # Enable the GNOME Desktop Environment.
+  # services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable Mouse in Hyprland
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+
+  # Enable Hyprlnd
+  programs.hyprland.enable = true;
+
+  # Enable xfce dbus communication (for Thunar)
+  services.dbus.packages = with pkgs; [
+    xfce.xfconf
+  ];
+
+  # Fonts Packages
+  fonts.packages = with pkgs; [
+    font-awesome
+  ];
+
+  # Enable Fonts
+  fonts.fontconfig.enable = true;
+
+  # Enable GVfs
+  services.gvfs.enable = true;
+
+  # Enable PAM Config for Swaylock-effects
+  security.pam.services.swaylock = {
+    text = ''
+      auth include login
+    '';
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
+    wireplumber.enable = true;
+    extraConfig.pipewire = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 32;
+        "default.clock.min-quantum" = 32;
+        "default.clock.max-quantum" = 32;
+      };
+    };    
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+  services.libinput.touchpad.tapping = false;  # This disables tap-to-click entirely.
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    ntfs3g
+    sdrpp
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+    docker-compose # start group of containers for dev
+    #podman-compose # start group of containers for dev
+  ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.user = {
+    isNormalUser = true;
+    description = "user";
+    extraGroups = [ 
+      "networkmanager"
+      "wheel"
+      "libvirtd" #Necessary for virt-manager
+    ];
+  };
+
+  # Virtualization Settings
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu.ovmf.enable = false;
+    };
+
+    containers.enable = true;
+
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  programs.virt-manager.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
