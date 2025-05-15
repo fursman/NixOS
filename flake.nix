f96622d1b9eecff4d375e569750cfac9651fb074
fursman <120282785+fursman@users.noreply.github.com>
2025-05-14 18:12:48 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -623,20 +623,6 @@
         home.sessionVariables.GTK_THEME = "Dracula";
 
         programs.firefox.enable = true;
-
-        # Enable common container config files in /etc/containers
-        virtualisation.containers.enable = true;
-        virtualisation = {
-          podman = {
-            enable = true;
-      
-            # Create a `docker` alias for podman, to use it as a drop-in replacement
-            dockerCompat = true;
-      
-            # Required for containers under podman-compose to be able to talk to each other.
-            defaultNetwork.settings.dns_enabled = true;
-          };
-        };
       
         home.packages = with pkgs; [
 
@@ -886,14 +872,27 @@
           "libvirtd" #Necessary for virt-manager
         ];
       };
-    
+
       # Virtualization Settings
       virtualisation = {
         libvirtd = {
           enable = true;
           qemu.ovmf.enable = false;
         };
+
+        containers.enable = true;
+  
+        podman = {
+          enable = true;
+    
+          # Create a `docker` alias for podman, to use it as a drop-in replacement
+          dockerCompat = true;
+    
+          # Required for containers under podman-compose to be able to talk to each other.
+          defaultNetwork.settings.dns_enabled = true;
+        };
       };
+
       programs.virt-manager.enable = true;
     
       # Some programs need SUID wrappers, can be configured further or are
